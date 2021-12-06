e67cff1b1086cce85f51aab8d6b87f7d46b543a1
Sylvain <dev.tess.sylvain@gmail.com>
2021-12-06 17:57:01 +0100

    Ad directory controllers + models and create user models and controllers. Implementation of Bcrypt

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user.route');
 require('dotenv').config({path: './config/.env'});
 require('./config/db')
 const app = express();
 
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Routes
+app.use('/api/user', userRoutes);
+
+
+//Server
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`)
 })
