b1fc0d567a503467caf6423ce1e513c9de088dc1
Sylvain <dev.tess.sylvain@gmail.com>
2021-12-06 15:08:28 +0100

    First Commit

---
+++ Backend/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://'+ process.env.DB_USER_PASSWORD +'@cluster0.akwyw.mongodb.net/MERN-Project',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: false
+    })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("Failed to connect to MongoDB", err));
