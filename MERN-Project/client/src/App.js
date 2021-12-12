44af6932c21856e58c7c4f28cff42fa9d1a5fcf2
Sylvain <dev.tess.sylvain@gmail.com>
2021-12-12 14:15:20 +0100

    Fin de la mise en place from

--- MERN-Project/client/src/App.js
+++ MERN-Project/client/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import Routes from "./components/routes";
 
 const App = () => {
     return (
-        <div>
-          Hello depuis app
-        </div>
+        <Routes />
     );
 };
 
---
+++ MERN-Project/client/src/components/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom';
+import Home from "../../pages/home";
+import Profil from "../../pages/profil";
+import Trending from "../../pages/trending";
+
+const Index = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Home/>}/>
+                <Route exact path="/profil" element={<Profil/>}/>
+                <Route exact path="/trending" element={<Trending/>}/>
+                <Route path="*" element={<Navigate to ="/" />}/>
+            </Routes>
+
+        </Router>
+    );
+};
+
+export default Index;
