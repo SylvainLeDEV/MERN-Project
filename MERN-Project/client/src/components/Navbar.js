a778c25512ce0f11642c7e38f0edca903f867fa7
Sylvain <dev.tess.sylvain@gmail.com>
2021-12-15 12:04:05 +0100

    Ajout leftNav mais il y a un bug sur le style. Ajout du profile avec l'uploasImage Mais l'image ne s'affiche pas il a un bug avec le back

--- MERN-Project/client/src/components/Navbar.js
+++ MERN-Project/client/src/components/Navbar.js
@@ -2,11 +2,11 @@ import React, {useContext} from 'react';
 import {NavLink} from "react-router-dom";
 import {UidContext} from "./AppContext";
 import Logout from "./log/Logout";
+import {useSelector} from "react-redux";
 
 const Navbar = () => {
     const uid = useContext(UidContext);
-
-    
+    const userData = useSelector((state => state.userReducer));
 
     return (
         <nav>
@@ -24,7 +24,7 @@ const Navbar = () => {
                         <li></li>
                         <li className="welcome">
                             <NavLink exact to="/profil">
-                                <h5>Bienvenue "Valeur dynamique"</h5>
+                                <h5>Bienvenue {userData.pseudo}</h5>
                             </NavLink>
                         </li>
                         <Logout />
